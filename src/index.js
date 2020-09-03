8c1c27f639c3b34096bd110174f3350b46fd6d3b
Hojung <hojung1111@gmail.com>
2020-09-02 20:11:44 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReacDOM from 'react-dom'
+
+import "./index.css"
+
+import App from './js/components/App'
+
+ReacDOM.render( <App />, document.getElementById( 'container' ) )
---
+++ src/js/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import './App.css'
+
+class App extends Component {
+
+  render () {
+    return (
+      <h1>React with Webpack and Babel</h1>
+    );
+  }
+}
+
+export default App;
