fefef412ee63009feb7716823959bf143605bed9
Hojung <hojung1111@gmail.com>
2020-09-14 22:38:47 -0400

    changed initial style

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -5,7 +5,11 @@ class App extends Component {
 
   render () {
     return (
-      <h1>React with Webpack and Babel</h1>
+      <main>
+        <h1>React with Webpack and Babel</h1>
+        <p>run <span className="code-block">npm run start</span> to start local dev server</p>
+        <p>run <span className="code-block">npm build</span> to compile your project</p>
+      </main>
     );
   }
 }
